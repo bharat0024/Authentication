c1701f865379a3631164fe50f64c72aed5ee8d9c
Bharat <makwana.bharat.2024@gmail.com>
2024-04-04 18:57:20 +0530

    removing defult port specification for mysql connection

--- config/db.ts
+++ config/db.ts
@@ -8,7 +8,6 @@ const connection = async () => {
     user: process.env.DB_USER,
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD,
-    port: 3306,
   });
 };
 export default connection;
